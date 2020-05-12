4e08e3e202da6fcb1af48e17ae198428b5fa01d3
FreseFea <fedor.palexov2005@gmail.com>
2020-05-12 17:58:00 +0300

    first commit

---
+++ app/src/main/java/com/example/calkulator/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.calkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+}
