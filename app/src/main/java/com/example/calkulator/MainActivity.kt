d78d9646e54f5ffb271326febd2f91939aae998c
FreseFea <fedor.palexov2005@gmail.com>
2020-05-15 18:22:56 +0300

    Вот

--- app/src/main/java/com/example/calkulator/MainActivity.kt
+++ app/src/main/java/com/example/calkulator/MainActivity.kt
@@ -5,6 +5,33 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_0.setOnClickListener{setTextFields("0")}
+        btn_1.setOnClickListener{setTextFields("1")}
+        btn_2.setOnClickListener{setTextFields("2")}
+        btn_3.setOnClickListener{setTextFields("3")}
+        btn_4.setOnClickListener{setTextFields("4")}
+        btn_5.setOnClickListener{setTextFields("5")}
+        btn_6.setOnClickListener{setTextFields("6")}
+        btn_7.setOnClickListener{setTextFields("7")}
+        btn_8.setOnClickListener{setTextFields("8")}
+        btn_9.setOnClickListener{setTextFields("9")}
+        minus_btn.setOnClickListener{setTextFields("-")}
+        plus_btn.setOnClickListener{setTextFields("+")}
+        mult_btn.setOnClickListener{setTextFields("*")}
+        desivion_btn.setOnClickListener{setTextFields("/")}
+        bracket_open_btn.setOnClickListener{setTextFields("(")}
+        bracket_close_btn.setOnClickListener{setTextFields(")")}
+
+
+
+    }
+    fun setTextFields(str:String){
+        math_operation.append(str)
+    }
+
 
 
 }
