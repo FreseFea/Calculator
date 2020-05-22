facb9af978cd799366c1a37549d5c2052d02e9a8
FreseFea <fedor.palexov2005@gmail.com>
2020-05-22 17:36:30 +0300

    Вот

--- app/src/main/java/com/example/calkulator/MainActivity.kt
+++ app/src/main/java/com/example/calkulator/MainActivity.kt
@@ -37,132 +37,13 @@ class MainActivity : AppCompatActivity() {
             result_text.text = "" }
 
 
-        <dependency>
-        <groupId>net.objecthunter</groupId>
-        <artifactId>exp4j</artifactId>
-        <version>0.4.8</version>
-        </dependency>
-
-
-        val e: Expression = ExpressionBuilder("3 * sin(y) - 2 / (x - 2)")
-            .variables("x", "y")
+        equal_btn.setOnClickListener{
+            val e: math_operation = ExpressionBuilder("3 * sin(y) - 2 / (x - 2)")
             .build()
-            .setVariable("x", 2.3)
-            .setVariable("y", 3.14)
-        val result: Double = e.evaluate()
-
-
-        val exec: ExecutorService = Executors.newFixedThreadPool(1)
-        val e: Expression = ExpressionBuilder("3log(y)/(x+1)")
-            .variables("x", "y")
-            .build()
-            .setVariable("x", 2.3)
-            .setVariable("y", 3.14)
-        val future: Future<Double> = e.evaluateAsync(exec)
-        val result: Double = future.get()
-
-
-        val result = ExpressionBuilder("2cos(xy)")
-            .variables("x", "y")
-            .build()
-            .setVariable("x", 0.5)
-            .setVariable("y", 0.25)
-            .evaluate()
-        assertEquals(2.0 * Math.cos(0.5 * 0.25), result, 0.0)
-
-
-        val expr = "pi+π+e+φ"
-        val expected = 2 * Math.PI + Math.E + 1.61803398874
-        val e: Expression = ExpressionBuilder(expr).build()
-        assertEquals(expected, e.evaluate(), 0.0)
-
-
-        val expr = "7.2973525698e-3"
-        val expected = expr.toDouble()
-        val e: Expression = ExpressionBuilder(expr).build()
-        assertEquals(expected, e.evaluate(), 0.0)
-
-
-        val logb: Function = object : Function("logb", 2) {
-            fun apply(vararg args: Double): Double {
-                return Math.log(args[0]) / Math.log(args[1])
-            }
-        }
-        val result = ExpressionBuilder("logb(8, 2)")
-            .function(logb)
-            .build()
-            .evaluate()
-        val expected = 3.0
-        assertEquals(expected, result, 0.0)
-
-
-        val avg: Function = object : Function("avg", 4) {
-            fun apply(vararg args: Double): Double {
-                var sum = 0.0
-                for (arg in args) {
-                    sum += arg
-                }
-                return sum / args.size
-            }
-        }
-        val result = ExpressionBuilder("avg(1,2,3,4)")
-            .function(avg)
-            .build()
-            .evaluate()
-
-        val expected = 2.5
-        assertEquals(expected, result, 0.0)
-
-
-        val factorial: Operator = object :
-            Operator(
-                "!",
-                1,
-                true,
-                PRECEDENCE_POWER + 1
-            ) {
-            override fun apply(vararg args: Double): Double {
-                val arg = args[0].toInt()
-                require(arg.toDouble() == args[0]) { "Operand for factorial has to be an integer" }
-                require(arg >= 0) { "The operand of the factorial can not be less than zero" }
-                var result = 1.0
-                for (i in 1..arg) {
-                    result *= i.toDouble()
-                }
-                return result
-            }
+            val result: Double = e.result_text()
         }
 
 
-        val result = ExpressionBuilder("3!")
-            .operator(factorial)
-            .build()
-            .evaluate()
-
-        val expected = 6.0
-        assertEquals(expected, result, 0.0)
-
-
-        @Test
-        public void testOperators3() throws Exception {
-            Operator gteq = new Operator(">=", 2, true, Operator.PRECEDENCE_ADDITION - 1) {
-
-                @Override
-                public double apply(double[] values) {
-                    if (values[0] >= values[1]) {
-                        return 1d;
-                    } else {
-                        return 0d;
-                    }
-                }
-            };
-
-            Expression e = new ExpressionBuilder("1>=2").operator(gteq)
-                .build();
-            assertTrue(0d == e.evaluate());
-            e = new ExpressionBuilder("2>=1").operator(gteq)
-                .build();
-            assertTrue(1d == e.evaluate());
 
 
 
